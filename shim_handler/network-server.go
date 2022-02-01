bc7e4b25bc527ce86460bc3b1654df66e09828db
MdbookTech <michael@mdbooktech.com>
2022-02-01 17:05:51 -0500

    Add files via upload

---
+++ shim_handler/network-server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+    "net"
+	"strings"
+	"log"
+	//"strconv"
+	"fmt"
+	//"bufio"
+    )
+
+
+func main () {
+	for {
+		GetPort()
+	}
+	
+	//fmt.Printf(string(GetOutboundIP()))
+
+}
+
+func GetPort(){
+	getPort,_ := net.Listen("tcp", "192.168.3.6:5003")
+	//defer getPort.Close()
+	conn, _ := getPort.Accept()
+	remoteIp := conn.RemoteAddr().String()
+	fmt.Printf("Received request from %s\n", remoteIp)
+	
+	remoteIpForm := remoteIp[:strings.Index(remoteIp, ":")]
+	remotePort := strings.ReplaceAll(remoteIpForm, ".", "")
+	remotePort = "2" + remotePort[len(remotePort)-4:]
+	conn.Write([]byte(remotePort))
+	//remotePortInt, _ := strconv.Atoi(remotePort)
+	fmt.Printf("Sent port %s to %s\n\n", remotePort, remoteIp)
+	conn.Close()
+	getPort.Close()
+	return
+}
+
+
+func GetOutboundIP() string {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+    ip := localAddr.IP
+	ipstr := ip.String()
+	ipstr = strings.ReplaceAll(ipstr, ".", "")
+	return ipstr
+}
