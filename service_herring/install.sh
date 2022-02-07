0718e7f69f11152165d11a694b18ff16e439189f
MdbookTech <michael@mdbooktech.com>
2022-02-07 14:07:28 -0500

    Added path changing

--- service_herring/install.sh
+++ service_herring/install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 #Build payloads
+cd downloader
 go build downloader/downloader.go
+cd ../file-creator
 go build file-creater/file-creator.go
+cd ../random-messenger
 go build random-messenger/random-messenger.go
+cd ../shell
 go build shell/shell.go
+cd ../user-creator
 go build user-creator/user-creator.go
+cd ..
 go build service-creator.go
