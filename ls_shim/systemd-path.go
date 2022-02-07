8e25709216e2dfe599697ba2ac15bbc0ec848c4d
MdbookTech <michael@mdbooktech.com>
2022-02-06 21:56:57 -0500

    Bug fix

--- ls_shim/systemd-path.go
+++ ls_shim/systemd-path.go
@@ -57,7 +57,7 @@ func EstablishConnection(port string) {
 func CheckFileExists(file string) bool {
 	//Can use the following if go is actually up to date
 	//if _, err := os.Stat(file); errors.Is(err, os.ErrNotExist) {
-	if _, err := os.Stat("file-exists.go"); err != nil {
+	if _, err := os.Stat(file); err == nil {
 		return false
 	}
 	return true
