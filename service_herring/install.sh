95b4e8b160a6eef5056c3deb9b88b738f0ff81e4
MdbookTech <michael@mdbooktech.com>
2022-02-07 14:12:08 -0500

    Build debug messages

--- service_herring/install.sh
+++ service_herring/install.sh
@@ -3,14 +3,22 @@
 #Build payloads
 cd downloader
 go build downloader.go
+echo "Built downloader"
 cd ../file-creator
 go build file-creator.go
+echo "Built file-creator"
 cd ../random-messenger
 go build random-messenger.go
+echo "Built random-messenger"
 cd ../shell
 go build shell.go
+echo "Built shell"
 cd ../user-creator
 go build user-creator.go
+echo "Built user-creator"
 cd ..
 go build service-creator.go
+echo "Built service-creator"
+
 echo "Built go files"
+
