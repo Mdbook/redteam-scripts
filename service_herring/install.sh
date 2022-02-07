30c812f3781bfdf7265899e4728d1e0f3d1e0d72
MdbookTech <michael@mdbooktech.com>
2022-02-07 14:08:36 -0500

    Fixed paths

--- service_herring/install.sh
+++ service_herring/install.sh
@@ -2,14 +2,14 @@
 
 #Build payloads
 cd downloader
-go build downloader/downloader.go
+go build downloader.go
 cd ../file-creator
-go build file-creater/file-creator.go
+go build file-creator.go
 cd ../random-messenger
-go build random-messenger/random-messenger.go
+go build random-messenger.go
 cd ../shell
-go build shell/shell.go
+go build shell.go
 cd ../user-creator
-go build user-creator/user-creator.go
+go build user-creator.go
 cd ..
 go build service-creator.go
