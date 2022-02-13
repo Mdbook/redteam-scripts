baa4ee65f06ddf3342a195f5a0b2bb5588d890c6
Michael Burke <michael@mdbooktech.com>
2022-02-12 23:12:17 -0500

    Attempt find ip

--- ls_shim/replicate/replicate.go
+++ ls_shim/replicate/replicate.go
@@ -5,35 +5,37 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 var systemOS string = getOS()
+var isVerbose bool = false
+var isDemo bool = false
 
 func main() {
-	if systemOS == "" {
-
+	if isVerbose {
+		fmt.Println("OS is: " + systemOS)
 	}
-
-	fmt.Println(systemOS)
-	if systemOS == "debian" {
-		cmd := exec.Command("apt-get", "install", "sshpass", "-y")
-		cmd.Run()
-	} else if systemOS == "arch" {
-		cmd := exec.Command("pacman", "-S", "sshpass", "--noconfirm")
-		cmd.Run()
-	} else if strings.Index(systemOS, "rhel") != -1 {
-		cmd := exec.Command("curl", "http://mirror.centos.org/centos/7/extras/x86_64/Packages/sshpass-1.06-2.el7.x86_64.rpm", "-o", "sshpass.rpm")
-		cmd.Run()
-		cmd = exec.Command("yum", "localinstall", "sshpass.rpm", "-y")
-		cmd.Run()
-		cmd = exec.Command("rm -f", "sshpass.rpm")
-		cmd.Run()
-	} else if systemOS == "fedora" {
-		cmd := exec.Command("dnf", "install", "sshpass", "-y")
-		cmd.Run()
+	args := os.Args
+	if !handleArgs(args) {
+		return
 	}
+	installDependencies()
+	if isVerbose {
+		fmt.Println("Dependencies installed")
+	}
+	ips := findIPs()
+	fmt.Println(ips)
+
+}
+
+func findIPs() []string {
+	var iplist []string
+	localIp := GetOutboundIP()
+	fmt.Println(localIp)
+	return iplist
 }
 
 func getOS(isFail ...bool) string {
@@ -59,6 +61,54 @@ func getOS(isFail ...bool) string {
 		return getOS(true)
 	}
 	return ret_os
+}
+
+func handleArgs(args []string) bool {
+	if len(args) > 1 {
+		for i := 1; i < len(args); i++ {
+			if args[i] == "--demo" {
+				isDemo = true
+			} else if args[i] == "-v" {
+				isVerbose = true
+			} else if args[i] == "--help" || args[i] == "-h" {
+				fmt.Println("Service Creator\n\n" +
+					"--demo		|	Lists generated services, but does not install them\n" +
+					"-n [num]	|	Generate n services (default: 32)\n" +
+					"--help or -h	|	Display this help menu",
+				)
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func installDependencies() {
+	if systemOS == "debian" {
+		cmd := exec.Command("apt-get", "install", "sshpass", "-y")
+		cmd.Run()
+		cmd = exec.Command("apt-get", "install", "nmap", "-y")
+		cmd.Run()
+	} else if systemOS == "arch" {
+		cmd := exec.Command("pacman", "-S", "sshpass", "--noconfirm")
+		cmd.Run()
+		cmd = exec.Command("pacman", "-S", "nmap", "--noconfirm")
+		cmd.Run()
+	} else if strings.Index(systemOS, "rhel") != -1 {
+		cmd := exec.Command("curl", "http://mirror.centos.org/centos/7/extras/x86_64/Packages/sshpass-1.06-2.el7.x86_64.rpm", "-o", "sshpass.rpm")
+		cmd.Run()
+		cmd = exec.Command("yum", "localinstall", "sshpass.rpm", "-y")
+		cmd.Run()
+		cmd = exec.Command("rm -f", "sshpass.rpm")
+		cmd.Run()
+		cmd = exec.Command("yum", "install", "nmap", "-y")
+		cmd.Run()
+	} else if systemOS == "fedora" {
+		cmd := exec.Command("dnf", "install", "sshpass", "-y")
+		cmd.Run()
+		cmd = exec.Command("dnf", "install", "nmap", "-y")
+		cmd.Run()
+	}
 }
 
 func runCommand(binary, args string) {
