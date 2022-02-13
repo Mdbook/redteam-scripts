5321372d9ffc15e9ca13615a3d60fbe6c86e4557
Michael Burke <michael@mdbooktech.com>
2022-02-13 18:18:55 -0500

    added threading

--- shim_handler/multi-handler.go
+++ shim_handler/multi-handler.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	args := os.Args
 	for i := 1; i < len(args); i++ {
 		ip := args[i]
-		do(ip)
+		wg.Add(1)
+		go do(ip)
 	}
+	wg.Wait()
+	fmt.Println("Finished!")
 }
 
 func do(ip string) {
+	defer wg.Done()
 	listenPort := strings.ReplaceAll(ip, ".", "")
 	listenPort = "2" + listenPort[len(listenPort)-4:]
 	cmd := exec.Command("xterm", "-title", ip, "-e", "nc", "-l", "-p", listenPort)
