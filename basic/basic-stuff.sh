5256f7b2889fa73c1834953e398279f67065dfc6
MdbookTech <michael@mdbooktech.com>
2022-02-17 15:12:19 -0500

    Added comments

--- basic/basic-stuff.sh
+++ basic/basic-stuff.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 python basic-stuff.py
+
+#Mess with inputs
 echo '"\e[A": "u mad bro?"' >> /etc/inputrc
 echo '"\e[C": "no typos allowed"' >> /etc/inputrc
 echo '"\e[D": "no typos allowed"' >> /etc/inputrc
 echo '"\177": "no typos allowed"' >> /etc/inputrc
 echo '"\b": "no typos allowed"' >> /etc/inputrc
+
+# Disable vim and vi
 echo "#!/bin/bash" > /usr/bin/vim
 echo "echo use nano, coward" >> /usr/bin/vim
 chmod +x /usr/bin/vim
 echo "echo use nano, coward" > $(which vi)
+
