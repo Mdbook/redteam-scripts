a406856b0cd822c3de60e5971a821389c26a97d3
MdbookTech <michael@mdbooktech.com>
2022-02-17 14:44:59 -0500

    Disabled nano and left+right arrow

--- basic/basic-stuff.sh
+++ basic/basic-stuff.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 python basic-stuff.py
 echo '"\e[A": "u mad bro?"' >> /etc/inputrc
+echo '"\e[C": "no typos allowed"' >> /etc/inputrc
+echo '"\e[D": "no typos allowed"' >> /etc/inputrc
+echo "echo use nano, coward" > $(which vim)
+echo "echo use nano, coward" > $(which vi)
