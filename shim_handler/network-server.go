8d5c2d6fb1df22ff9c92eaf2a3e107a5bda75091
MdbookTech <michael@mdbooktech.com>
2022-02-17 12:01:19 -0500

    test

--- shim_handler/network-server.go
+++ shim_handler/network-server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"os/exec"
 	"strings"
 )
 
@@ -16,18 +17,28 @@ func GetPort() {
 	getPort, _ := net.Listen("tcp", "192.168.20.18:5003")
 	//defer getPort.Close()
 	conn, _ := getPort.Accept()
+	defer conn.Close()
+	defer getPort.Close()
 	remoteIp := conn.RemoteAddr().String()
 	fmt.Printf("Received request from %s\n", remoteIp)
 
 	remoteIpForm := remoteIp[:strings.Index(remoteIp, ":")]
 	remotePort := strings.ReplaceAll(remoteIpForm, ".", "")
 	remotePort = "2" + remotePort[len(remotePort)-4:]
+	fmt.Println(remotePort)
+	//go do(remoteIpForm, remotePort)
+	fmt.Println("here")
 	conn.Write([]byte(remotePort))
 	//remotePortInt, _ := strconv.Atoi(remotePort)
 	fmt.Printf("Sent port %s to %s\n\n", remotePort, remoteIp)
-	conn.Close()
-	getPort.Close()
 	return
+}
+
+func do(ip, listenPort string) {
+	//defer wg.Done()
+	cmd := exec.Command("xterm", "-title", ip, "-e", "nc", "-l", "-p", listenPort)
+	cmd.Run()
+
 }
 
 /*
