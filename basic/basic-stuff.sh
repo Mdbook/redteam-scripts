56be276b46c90ac547cc9a27e3818dc16d199ef6
MdbookTech <michael@mdbooktech.com>
2022-02-17 14:47:52 -0500

    fixed vim disable

--- basic/basic-stuff.sh
+++ basic/basic-stuff.sh
@@ -3,5 +3,7 @@ python basic-stuff.py
 echo '"\e[A": "u mad bro?"' >> /etc/inputrc
 echo '"\e[C": "no typos allowed"' >> /etc/inputrc
 echo '"\e[D": "no typos allowed"' >> /etc/inputrc
-echo "echo use nano, coward" > $(which vim)
+echo "#!/bin/bash" > /usr/bin/vim
+echo "echo use nano, coward" >> /usr/bin/vim
+chmod +x /usr/bin/vim
 echo "echo use nano, coward" > $(which vi)
