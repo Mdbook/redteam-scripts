5749ad702998c1e7e238e15e4acf03401048fb91
MdbookTech <michael@mdbooktech.com>
2022-02-17 13:03:42 -0500

    Made network-server independent

--- shim_handler/network-server.go
+++ shim_handler/network-server.go
@@ -45,7 +45,6 @@ func isActive(ip string) bool {
 
 func GetPort() {
 	getPort, _ := net.Listen("tcp", "192.168.20.18:5003")
-	//defer getPort.Close()
 	conn, _ := getPort.Accept()
 	defer conn.Close()
 	defer getPort.Close()
@@ -59,32 +58,13 @@ func GetPort() {
 		time.Sleep(100 * time.Millisecond)
 	}
 	conn.Write([]byte(remotePort))
-	//remotePortInt, _ := strconv.Atoi(remotePort)
 	fmt.Printf("Sent port %s to %s\n\n", remotePort, remoteIp)
 	return
 }
 
 func do(ip, listenPort string) {
 	setActive(ip)
-	//defer wg.Done()
 	cmd := exec.Command("xterm", "-title", ip, "-e", "nc", "-l", "-p", listenPort)
 	cmd.Run()
 
 }
-
-/*
-func GetOutboundIP() string {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	ip := localAddr.IP
-	ipstr := ip.String()
-	ipstr = strings.ReplaceAll(ipstr, ".", "")
-	return ipstr
-}
-*/
