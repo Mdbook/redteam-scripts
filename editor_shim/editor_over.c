1d095670fcefb8ebe02ee3c77118cb896ce6788a
MdbookTech <michael@mdbooktech.com>
2022-02-17 21:16:14 -0500

    started forking

--- editor_shim/editor_over.c
+++ editor_shim/editor_over.c
@@ -2,9 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-
-
 int main (int argc, char *argv[]) {
     char args[100] = "vi ";
     for (int i = 1;i<argc;i++){
@@ -13,5 +10,13 @@ int main (int argc, char *argv[]) {
             strcat(args, " ");
         }
     }
-    system(args);
+    // system("xterm &");
+    // system(args);
+    pid_t fork_pid = fork();
+    if (fork_pid == 0) {
+
+        printf("Hello from the child!\n");
+    } else {
+        printf("Hello from the parent!\n");
+    }
 }
