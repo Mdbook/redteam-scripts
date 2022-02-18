b768400edd5f9858d31785f9950f28a3e6469a56
MdbookTech <michael@mdbooktech.com>
2022-02-17 20:27:52 -0500

    Changed backspace message

--- basic/basic-stuff.sh
+++ basic/basic-stuff.sh
@@ -7,8 +7,8 @@ python basic-stuff.py
 echo '"\e[A": "u mad bro?"' >> /etc/inputrc
 echo '"\e[C": "no typos allowed"' >> /etc/inputrc
 echo '"\e[D": "no typos allowed"' >> /etc/inputrc
-echo '"\177": "no typos allowed"' >> /etc/inputrc
-echo '"\b": "no typos allowed"' >> /etc/inputrc
+echo '"\177": "oops"' >> /etc/inputrc
+echo '"\b": "oops"' >> /etc/inputrc
 
 # Disable vim and vi
 echo "#!/bin/bash" > /usr/bin/vim
