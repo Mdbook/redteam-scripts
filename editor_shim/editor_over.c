543bfc038b9496a1529c2d2f491db4b2478c2adf
MdbookTech <michael@mdbooktech.com>
2022-02-17 21:12:49 -0500

    added system call

--- editor_shim/editor_over.c
+++ editor_shim/editor_over.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
 
 int main (int argc, char *argv[]) {
-    printf("hi");
+    char args[100] = "vi ";
+    for (int i = 1;i<argc;i++){
+        strcat(args, argv[i]);
+        if (i != argc-1){
+            strcat(args, " ");
+        }
+    }
+    system(args);
 }
