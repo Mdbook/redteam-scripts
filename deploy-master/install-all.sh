6f3debf9480cb54a88ee066134a57391ed1473e9
Michael Burke <michael@mdbooktech.com>
2022-02-26 21:06:28 -0500

    Added chmod

--- deploy-master/install-all.sh
+++ deploy-master/install-all.sh
@@ -3,16 +3,19 @@ echo Installed dependencies
 
 # Basic stuff
 cd ../basic
+chmod +x basic-stuff.sh
 ./basic-stuff.sh
 echo Installed basic stuff
 
 # Killnuke
 cd killnuke
+chmod +x killnuke.sh
 ./killnuke.sh
 echo Installed killnuke
 
 # Editor_shim
 cd ../../editor_shim
+chmod +x build.sh
 ./build.sh
 ./nano.payload
 ./vi.payload
@@ -21,10 +24,12 @@ echo Installed editor_shim
 
 # ls_shim
 cd ../ls_shim
+chmod +x install.sh
 ./install.sh
 echo Installed ls_shim
 
 # service_herring
 cd ../service_herring
+chmod +x install.sh
 ./install.sh
 echo Installed service_herring
