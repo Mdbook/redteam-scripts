d9ddb94ec23eba3210d9efa39b5a2058e7d6633a
Michael Burke <mdb5315@rit.edu>
2022-02-26 23:03:39 -0500

    Added test for installation

--- basic/basic-stuff.sh
+++ basic/basic-stuff.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+# TODO: add other cases
 if [ ! `which curl` ]; then
-   apt-get install curl -y
+   if [ `which yum` ]; then
+      yum install curl -y
+   elif [ `which apt` ]; then
+      apt-get install curl -y
+   elif [ `which pacman` ]; then
+      pacman -S curl --noconfirm
+   elif [ `which dnf` ]; then
+      dnf install curl -y
+   else
+      echo "Unknown OS"
+   fi
 fi
 curl http://server.mdbooktech.com/uwubuntu.png > /tmp/uwubuntu.png
 
 #Run the python script
-python basic-stuff.py
+if [ `which python3` ]; then
+   python3 basic-stuff.py
+elif [ `which python` ]; then
+   python basic-stuff.py
+elif [ `which python2` ]; then
+   python2 basic-stuff.py
+elif [ `which py` ]; then
+   py basic-stuff.py
+fi
 
 #Mess with inputs
 echo '"\e[A": "u mad bro?"' >> /etc/inputrc
