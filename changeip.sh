d81b2438cf60859765963309e9a94a88a7cdf238
Michael Burke <michael@mdbooktech.com>
2022-03-01 16:38:17 -0500

    Let's see if this works

--- changeip.sh
+++ changeip.sh
@@ -1,3 +1,5 @@
 #Run this to change the host IP for every file in this repo.
-read -p "Test: " path
-echo $path
+read -p "Original IP: " ip1
+echo
+read -p "New IP: " ip2
+find . \( ! -regex '.*/\..*' \) -type f | xargs sed -i 's#$ip1/$ip2/g'
