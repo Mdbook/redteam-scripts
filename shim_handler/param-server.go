a3f6545bf4a6ae9acd18e908c6a835f2a9abf758
MdbookTech <michael@mdbooktech.com>
2022-03-01 20:52:05 -0500

    Added random port generation

--- shim_handler/param-server.go
+++ shim_handler/param-server.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"time"
 )
 
 var port string
 var hasPort bool = false
+var takenPorts []string
 
 func main() {
 	handleArgs()
@@ -38,6 +41,14 @@ func handleArgs() {
 	fmt.Println("Listening on port " + port)
 }
 
+func getRandomPort() string {
+	remotePort := "2" + strconv.Itoa(random(99)) + strconv.Itoa(random(99))
+	if findIndex(takenPorts, remotePort) == -1 {
+		return remotePort
+	}
+	return getRandomPort()
+}
+
 func GetPort() {
 	getPort, _ := net.Listen("tcp", "192.168.20.18:"+port)
 	conn, _ := getPort.Accept()
@@ -46,8 +57,9 @@ func GetPort() {
 	remoteIp := conn.RemoteAddr().String()
 	fmt.Printf("Received request from %s\n", remoteIp)
 	remoteIpForm := remoteIp[:strings.Index(remoteIp, ":")]
-	remotePort := strings.ReplaceAll(remoteIpForm, ".", "")
-	remotePort = "2" + remotePort[len(remotePort)-4:]
+	remotePort := getRandomPort()
+	// remotePort := strings.ReplaceAll(remoteIpForm, ".", "")
+	// remotePort = "2" + remotePort[len(remotePort)-4:]
 	go do(remoteIpForm, remotePort)
 	time.Sleep(100 * time.Millisecond)
 	conn.Write([]byte(remotePort))
@@ -56,6 +68,32 @@ func GetPort() {
 }
 
 func do(ip, listenPort string) {
+	fmt.Println(takenPorts)
 	cmd := exec.Command("xterm", "-title", ip+" | "+port, "-e", "nc", "-l", "-p", listenPort)
 	cmd.Run()
+	takenPorts, _ = remove(takenPorts, findIndex(takenPorts, listenPort))
+}
+
+func random(n int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(n)
+}
+
+func remove(slice []string, i int) ([]string, string) {
+	//Remove an item from a slice
+	name := slice[i]
+	slice[i] = slice[len(slice)-1]
+	slice = slice[:len(slice)-1]
+	return slice, name
+
+}
+
+func findIndex(slice []string, value string) int {
+	//Find the index of a string in a slice
+	for i := range slice {
+		if slice[i] == value {
+			return i
+		}
+	}
+	return -1
 }
