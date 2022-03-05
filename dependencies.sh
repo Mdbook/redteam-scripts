0beca7dd2d37fd27a5a799b0c18ea1ff255aa390
MdbookTech <michael@mdbooktech.com>
2022-03-04 20:38:38 -0500

    Fixed dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 if [ `which yum` ]; then
    yum install golang -y
-elif [ `which apt` ]; then
+   yum install libcurl-devel -y
+elif [ `which apt-get` ]; then
    apt-get install golang -y
+   apt-get install libcurl4-openssl-dev -y
 elif [ `which pacman` ]; then
    pacman -S go --noconfirm
+   pacman -S curl --noconfirm
 elif [ `which dnf` ]; then
    dnf install golang -y
+   dnf install libcurl-devel -y
 else
    echo "Unknown OS"
 fi
